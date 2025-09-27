605a07a9c526f075296165d793c0d2f111254ed7
XuanNam2k4 <nam.hx.64cntt@ntu.edu.vn>
2025-09-27 13:48:36 +0700

    Add file php

---
+++ bangcuuchuong.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BT-BẢNG CỬU CHƯƠNG</title>
+</head>
+<style>
+        table,
+        tr,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+<body>
+    <table>
+        <?php
+        echo "<tr>";
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<td>";
+            echo "<b>Chuong" . $i . "</b>";
+            echo "</td>";
+        }
+        echo "</tr>";
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<tr>";
+            for ($j = 1; $j <= 10; $j++) {
+                $result = $i * $j;
+                $color = ($result % 2 == 0) ? "red":"green";
+                echo "<td style=' color: $color;'>";
+                echo $i . "*" . $j . "=".$result;
+                echo "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
